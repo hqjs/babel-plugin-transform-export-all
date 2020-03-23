93ebd3d005652e00981d25a59652027fe79a7614
Yuri <yuri.karadzhov@gmail.com>
2020-03-23 23:28:54 +0200

    Take care of export location

--- index.js
+++ index.js
@@ -9,17 +9,27 @@ const objectAssign = (t, names, obj = t.objectExpression([])) => t.callExpressio
   [obj, ...names]
 );
 
-const exportDefaultVisitor = (t, found, names) => ({
+const exportDefaultVisitor = (t, found, names, lastExport) => ({
   ExportDefaultDeclaration(nodePath) {
     const { declaration } = nodePath.node;
     if (isUndefinedOrVoid(t, declaration)) return;
-    nodePath.node.declaration = objectAssign(t, names, declaration);
-    found.value = true;
+    if (nodePath.node.start > lastExport.node.source.start) {
+      nodePath.node.declaration = objectAssign(t, names, declaration);
+      found.value = true;
+      nodePath.stop();
+      return;
+    } else {
+      nodePath.remove();
+      lastExport.insertAfter(t.exportDefaultDeclaration(objectAssign(t, names, declaration)));
+      found.value = true;
+      nodePath.stop();
+    }
   }
 });
 
 module.exports = function ({ types: t }) {
   const names = [];
+  let lastExport = null;
   return {
     visitor: {
       ExportAllDeclaration(nodePath) {
@@ -30,15 +40,17 @@ module.exports = function ({ types: t }) {
           source
         ));
         names.push(importName);
+        lastExport = nodePath;
       },
       Program: {
         enter() {
           names.length = 0;
+          lastExport = null;
         },
         exit(nodePath) {
           if (names.length === 0) return;
           const found = { value: false };
-          nodePath.traverse(exportDefaultVisitor(t, found, names));
+          nodePath.traverse(exportDefaultVisitor(t, found, names, lastExport));
           if (!found.value) {
             nodePath.node.body.push(t.exportDefaultDeclaration(objectAssign(t, names)));
           }
